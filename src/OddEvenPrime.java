cc77772ccf7d7414b8eb678bb92480e2b44f52ca
Brandon Teng <bteng22@gmail.com>
2014-12-16 23:51:23 -0600

    include prime numbers but poor implementation

--- src/OddEvenPrime.java
+++ src/OddEvenPrime.java
@@ -2,6 +2,24 @@ public class OddEvenPrime {
 
     public String evaluate(int number) {
 
+        if(number == 1) {
+            return "Odd";
+        } else if (number == 2) {
+            return "Prime";
+        } else if (number == 3) {
+            return "Prime";
+        }
+        
+        if(!isEven(number) && number % 3 != 0) {
+            return "Prime";
+        }
+
+//        for(int i = 2; i < number; i++) {
+//            if (isOdd(number) && number % i != 0 ) {
+//                return "Prime";
+//            }
+//        }
+
         if (isEven(number)) return "Even";
 
         if (isOdd(number)) return "Odd";

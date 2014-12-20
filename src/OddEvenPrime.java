d1e35134068429b15c696ad0c04978e9c5dba772
Brandon Teng <bteng22@gmail.com>
2014-12-20 14:20:50 -0600

    refactor solution

--- src/OddEvenPrime.java
+++ src/OddEvenPrime.java
@@ -2,29 +2,24 @@ public class OddEvenPrime {
 
     public String evaluate(int number) {
 
-        if(number == 1) {
-            return "Odd";
-        } else if (number == 2) {
-            return "Prime";
-        } else if (number == 3) {
-            return "Prime";
-        }
-        
-        if(!isEven(number) && number % 3 != 0) {
-            return "Prime";
-        }
+        if (number == 1) return "Odd";
 
-//        for(int i = 2; i < number; i++) {
-//            if (isOdd(number) && number % i != 0 ) {
-//                return "Prime";
-//            }
-//        }
+        if (isPrime(number)) return "Prime";
 
         if (isEven(number)) return "Even";
 
         if (isOdd(number)) return "Odd";
 
         return String.valueOf(number);
+    }
+
+    private boolean isPrime(int number) {
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private boolean isOdd(int number) {
